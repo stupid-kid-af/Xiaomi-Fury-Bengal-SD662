144f1c037ebe6d5bed9301bb7d53a3589ffec921
Ashutosh Tripathi <pushpatripathi1111@gmail.com>
2022-02-01 13:56:43 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -37,9 +37,8 @@ echo -e " "
 
 if ! [ -d "$TC_DIR" ]; then
 echo "Proton clang not found! Cloning to $TC_DIR..."
-if ! wget git clone https://github.com/javashin/Xiaomi-Fury-Bengal-SD662.git -b JuiceIcedSnow $TC_DIR; then
+if ! wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r437112b.tar.gz $TC_DIR; then
 echo "Cloning failed! Aborting..."
-tar xvpf clang-r437112b.tar.gz
 exit 1
 fi
 fi
